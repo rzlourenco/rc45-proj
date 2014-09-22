3c9ba85e69dd7b69582349eb854e389d1bd717ee
Francisco Calisto <francisco_mcalisto@hotmail.com>
2014-09-22 13:40:50 +0100

    Added basic loop for central server.

---
+++ central-server/main.c
@@ -0,0 +1,28 @@
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+
+
+int main(int argc, char *argv[]) {
+  // set up udp
+  // set up tcp
+
+  // bind
+  // listen
+
+  for (;;) {
+    // newfd = accept
+    // fork
+    // if parent
+    //   close newfd
+    //   continue
+    // handle newfd
+  }
+
+  return 0;
+}
+
